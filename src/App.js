b7a14c749f02d885fecc4df3f1773b8c94bf915d
unknown <Muhammad.Khalikov29@student.qcc.cuny.edu>
2020-09-06 10:09:55 -0400

    Formatted the files

--- src/App.js
+++ src/App.js
@@ -2,87 +2,87 @@ import React from 'react'
 import Table from './components/Table'
 
 export default class App extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            operation: '→',
-            error: false,
-        }
-        this.handleChange = this.handleChange.bind(this)
-    }
+	constructor(props) {
+		super(props)
+		this.state = {
+			operation: '→',
+			error: false,
+		}
+		this.handleChange = this.handleChange.bind(this)
+	}
 
-    changeSymbol = (symbol) => {
-        symbol = symbol.toLowerCase().replace(/ /g, '')
-        if (symbol === '&&' || symbol === '/\\' || symbol === 'and') {
-            return '∧'
-        } else if (
-            symbol === '||' ||
-            symbol === '\\/' ||
-            symbol === 'v' ||
-            symbol === 'or'
-        ) {
-            return '∨'
-        } else if (symbol === '->' || symbol === 'if') {
-            return '→'
-        } else if (symbol === '<->' || symbol === 'iff') {
-            return '⇔'
-        }
-    }
+	changeSymbol = (symbol) => {
+		symbol = symbol.toLowerCase().replace(/ /g, '')
+		if (symbol === '&&' || symbol === '/\\' || symbol === 'and') {
+			return '∧'
+		} else if (
+			symbol === '||' ||
+			symbol === '\\/' ||
+			symbol === 'v' ||
+			symbol === 'or'
+		) {
+			return '∨'
+		} else if (symbol === '->' || symbol === 'if') {
+			return '→'
+		} else if (symbol === '<->' || symbol === 'iff') {
+			return '⇔'
+		}
+	}
 
-    handleChange = (event) => {
-        event.target.value = event.target.value.toLowerCase().replace(/ /g, '')
-        let operations = [
-            '&&',
-            '||',
-            '\\/',
-            '/\\',
-            '->',
-            '<->',
-            'iff',
-            'if',
-            'or',
-            'and',
-        ]
-        if (operations.includes(event.target.value)) {
-            this.setState({
-                operation:
-                    event.target.value !== ''
-                        ? this.changeSymbol(event.target.value)
-                        : '→',
-                error: false,
-            })
-        } else if (event.target.value === '') {
-            this.setState({ operation: '→', error: false })
-        } else if (event.target.value.length >= 2) {
-            this.setState({ error: true })
-        }
-    }
+	handleChange = (event) => {
+		event.target.value = event.target.value.toLowerCase().replace(/ /g, '')
+		let operations = [
+			'&&',
+			'||',
+			'\\/',
+			'/\\',
+			'->',
+			'<->',
+			'iff',
+			'if',
+			'or',
+			'and',
+		]
+		if (operations.includes(event.target.value)) {
+			this.setState({
+				operation:
+					event.target.value !== ''
+						? this.changeSymbol(event.target.value)
+						: '→',
+				error: false,
+			})
+		} else if (event.target.value === '') {
+			this.setState({ operation: '→', error: false })
+		} else if (event.target.value.length >= 2) {
+			this.setState({ error: true })
+		}
+	}
 
-    render() {
-        return (
-            <div className="container">
-                <h1>Truth Table Builder</h1>
-                <p>
-                    Tool to generate a truth table for propositional logic.
-                    <br/>
-                    Enter logical operators by using /\, \/ (slashes) or the way they are
-                    used
-                    <br/> in programming which is && (AND), || (OR), ! (NOT)
-                </p>
-                <form>
-                    <input
-                        type="text"
-                        value={this.state.value}
-                        onChange={this.handleChange}
-                        placeholder="Enter an operation"
-                        maxLength="3"
-                    />
-                </form>
-                <Table operation={this.state.operation}/>
-                <div className="error">
-                    <h1>{this.state.error ? 'Please enter a valid operation' : ''}</h1>
-                </div>
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div className="container">
+				<h1>Truth Table Builder</h1>
+				<p>
+					Tool to generate a truth table for propositional logic.
+					<br />
+					Enter logical operators by using /\, \/ (slashes) or the way they are
+					used
+					<br /> in programming which is && (AND), || (OR), ! (NOT)
+				</p>
+				<form>
+					<input
+						type="text"
+						value={this.state.value}
+						onChange={this.handleChange}
+						placeholder="Enter an operation"
+						maxLength="3"
+					/>
+				</form>
+				<Table operation={this.state.operation} />
+				<div className="error">
+					<h1>{this.state.error ? 'Please enter a valid operation' : ''}</h1>
+				</div>
+			</div>
+		)
+	}
 }
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,50 +1,50 @@
 import React from 'react'
 
 const calculateValues = (operation) => {
-    if (operation === '→') {
-        return ['T', 'F', 'T', 'T']
-    } else if (operation === '⇔') {
-        return ['T', 'F', 'F', 'T']
-    } else if (operation === '∧') {
-        return ['T', 'F', 'F', 'F']
-    } else if (operation === '∨') {
-        return ['T', 'T', 'T', 'F']
-    }
+	if (operation === '→') {
+		return ['T', 'F', 'T', 'T']
+	} else if (operation === '⇔') {
+		return ['T', 'F', 'F', 'T']
+	} else if (operation === '∧') {
+		return ['T', 'F', 'F', 'F']
+	} else if (operation === '∨') {
+		return ['T', 'T', 'T', 'F']
+	}
 }
 
 const Table = (props) => {
-    const values = calculateValues(props.operation)
-    return (
-        <div>
-            <table>
-                <tr>
-                    <td>p</td>
-                    <td>q</td>
-                    <td>p {props.operation} q</td>
-                </tr>
-                <tr>
-                    <td>T</td>
-                    <td>T</td>
-                    <td>{values[0]}</td>
-                </tr>
-                <tr>
-                    <td>T</td>
-                    <td>F</td>
-                    <td>{values[1]}</td>
-                </tr>
-                <tr>
-                    <td>F</td>
-                    <td>T</td>
-                    <td>{values[2]}</td>
-                </tr>
-                <tr>
-                    <td>F</td>
-                    <td>F</td>
-                    <td>{values[3]}</td>
-                </tr>
-            </table>
-        </div>
-    )
+	const values = calculateValues(props.operation)
+	return (
+		<div>
+			<table>
+				<tr>
+					<td>p</td>
+					<td>q</td>
+					<td>p {props.operation} q</td>
+				</tr>
+				<tr>
+					<td>T</td>
+					<td>T</td>
+					<td>{values[0]}</td>
+				</tr>
+				<tr>
+					<td>T</td>
+					<td>F</td>
+					<td>{values[1]}</td>
+				</tr>
+				<tr>
+					<td>F</td>
+					<td>T</td>
+					<td>{values[2]}</td>
+				</tr>
+				<tr>
+					<td>F</td>
+					<td>F</td>
+					<td>{values[3]}</td>
+				</tr>
+			</table>
+		</div>
+	)
 }
 
 export default Table
